93c81563edda2733e87942462da8bc9ab3890303
einstein <einstein@DESKTOP-IR4MP60>
2021-10-20 15:08:23 -0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace zadanko_domowe
+
+{
+    class losowanie_liczb
+    {
+        public static void Main()
+        {
+            int liczby;
+            while ((liczby = int.Parse(Console.ReadLine())) != 42069)
+            {
+                int counter = 0;
+                int kulturoznawstwo = 0;
+                while (counter <= liczby)
+                {
+                    if (isPrime(kulturoznawstwo))
+                    {
+                        Console.Write(kulturoznawstwo + " 1");
+                        counter++;
+                    }
+                    kulturoznawstwo++;
+                }
+                Console.WriteLine();
+            }
+        }
+        static bool isPrime(int n)
+        {
+            if (n <= 1) return false;
+            for (int i = 2; i * i <= n; i++)
+            {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
